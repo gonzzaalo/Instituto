b29978688e4dcec61516423c108189e899319275
EzePor <ezeporche@gmail.com>
2024-10-21 18:11:11 -0300

    Reformateo de llaves en PostTurnoExamen
    Se ha ajustado el formato del código en el método `PostTurnoExamen` del controlador `ApiTurnosExamenesController` para mejorar la legibilidad. La llave de apertura `{` se ha movido a una nueva línea.

--- InstitutoBack/Controllers/MesasExamenes/ApiTurnosExamenesController.cs
+++ InstitutoBack/Controllers/MesasExamenes/ApiTurnosExamenesController.cs
@@ -78,7 +78,7 @@ namespace InstitutoBack.Controllers.MesasExamenes
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
         public async Task<ActionResult<TurnoExamen>> PostTurnoExamen(TurnoExamen turnoExamen)
-        {
+        { 
             _context.turnosexamenes.Add(turnoExamen);
             await _context.SaveChangesAsync();
 
