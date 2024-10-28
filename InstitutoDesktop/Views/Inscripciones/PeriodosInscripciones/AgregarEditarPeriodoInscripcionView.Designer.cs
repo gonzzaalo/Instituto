be24e8f804e74c1c20d435e49e61413d1b5c3a5f
EzePor <ezeporche@gmail.com>
2024-10-28 19:22:44 -0300

    Añadir vistas y recursos para PeriodoInscripcion
    Se han añadido varias directivas `using` en los archivos
    `AgregarEditarPeriodoInscripcionView.cs` y `PeriodoInscripcionView.cs`
    para incluir servicios y modelos necesarios.
    
    Se ha creado la clase parcial `AgregarEditarPeriodoInscripcionView`
    en el archivo `AgregarEditarPeriodoInscripcionView.cs` que hereda de
    `Form`, incluyendo variables privadas, constructores y métodos para
    cargar datos y manejar eventos de botones.
    
    Se ha añadido el archivo `AgregarEditarPeriodoInscripcionView.Designer.cs`
    que define el diseño del formulario `AgregarEditarPeriodoInscripcionView`,
    incluyendo controles como etiquetas, cuadros de texto, botones y checkboxes.
    
    Se ha añadido el archivo `AgregarEditarPeriodoInscripcionView.resx` que
    contiene recursos necesarios para el formulario `AgregarEditarPeriodoInscripcionView`.
    
    Se ha creado la clase parcial `PeriodoInscripcionView` en el archivo
    `PeriodoInscripcionView.cs` que hereda de `Form`, incluyendo variables
    privadas, un constructor y métodos para cargar la grilla de períodos de
    inscripción y manejar eventos de botones.
    
    Se ha añadido el archivo `PeriodoInscripcionView.Designer.cs` que define
    el diseño del formulario `PeriodoInscripcionView`, incluyendo controles
    como un `DataGridView` y botones.
    
    Se ha añadido un nuevo archivo de recursos en formato XML llamado
    `PeriodoInscripcionView.resx`, que sigue el esquema de Microsoft ResX
    versión 2.0 y contiene metadatos y datos de recursos.

---
+++ InstitutoDesktop/Views/Inscripciones/PeriodosInscripciones/AgregarEditarPeriodoInscripcionView.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace InstitutoDesktop.Views.Inscripciones
+{
+    partial class AgregarEditarPeriodoInscripcionView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            chInscripcionActiva = new CheckBox();
+            cboCiclosLectivos = new ComboBox();
+            label2 = new Label();
+            chMaterias2doCuatrimestre = new CheckBox();
+            LabelTitulo = new Label();
+            txtNombre = new TextBox();
+            btnSalir = new FontAwesome.Sharp.IconButton();
+            btnAgregar = new FontAwesome.Sharp.IconButton();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(27, 125);
+            label1.Name = "label1";
+            label1.Size = new Size(67, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Nombre:";
+            // 
+            // chInscripcionActiva
+            // 
+            chInscripcionActiva.AutoSize = true;
+            chInscripcionActiva.Location = new Point(100, 239);
+            chInscripcionActiva.Name = "chInscripcionActiva";
+            chInscripcionActiva.Size = new Size(147, 24);
+            chInscripcionActiva.TabIndex = 1;
+            chInscripcionActiva.Text = "Inscripcion Activa";
+            chInscripcionActiva.UseVisualStyleBackColor = true;
+            // 
+            // cboCiclosLectivos
+            // 
+            cboCiclosLectivos.FormattingEnabled = true;
+            cboCiclosLectivos.Location = new Point(515, 117);
+            cboCiclosLectivos.Name = "cboCiclosLectivos";
+            cboCiclosLectivos.Size = new Size(231, 28);
+            cboCiclosLectivos.TabIndex = 4;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(413, 125);
+            label2.Name = "label2";
+            label2.Size = new Size(96, 20);
+            label2.TabIndex = 3;
+            label2.Text = "Ciclo Lectivo:";
+            // 
+            // chMaterias2doCuatrimestre
+            // 
+            chMaterias2doCuatrimestre.AutoSize = true;
+            chMaterias2doCuatrimestre.Location = new Point(515, 239);
+            chMaterias2doCuatrimestre.Name = "chMaterias2doCuatrimestre";
+            chMaterias2doCuatrimestre.Size = new Size(208, 24);
+            chMaterias2doCuatrimestre.TabIndex = 5;
+            chMaterias2doCuatrimestre.Text = "Materias 2do Cuatrimentre";
+            chMaterias2doCuatrimestre.UseVisualStyleBackColor = true;
+            // 
+            // LabelTitulo
+            // 
+            LabelTitulo.AutoSize = true;
+            LabelTitulo.Font = new Font("Bahnschrift SemiCondensed", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            LabelTitulo.ForeColor = Color.Blue;
+            LabelTitulo.Location = new Point(222, 25);
+            LabelTitulo.Name = "LabelTitulo";
+            LabelTitulo.Size = new Size(421, 36);
+            LabelTitulo.TabIndex = 6;
+            LabelTitulo.Text = "Agregar/Editar Período Inscripción";
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(100, 117);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(252, 27);
+            txtNombre.TabIndex = 14;
+            // 
+            // btnSalir
+            // 
+            btnSalir.BackColor = Color.Gray;
+            btnSalir.ForeColor = Color.White;
+            btnSalir.IconChar = FontAwesome.Sharp.IconChar.File;
+            btnSalir.IconColor = Color.White;
+            btnSalir.IconFont = FontAwesome.Sharp.IconFont.Regular;
+            btnSalir.IconSize = 24;
+            btnSalir.ImageAlign = ContentAlignment.MiddleLeft;
+            btnSalir.Location = new Point(553, 317);
+            btnSalir.Margin = new Padding(3, 4, 3, 4);
+            btnSalir.Name = "btnSalir";
+            btnSalir.Padding = new Padding(15, 0, 0, 0);
+            btnSalir.Size = new Size(127, 69);
+            btnSalir.TabIndex = 16;
+            btnSalir.Text = "Salir";
+            btnSalir.UseVisualStyleBackColor = false;
+            btnSalir.Click += btnSalir_Click;
+            // 
+            // btnAgregar
+            // 
+            btnAgregar.BackColor = Color.Green;
+            btnAgregar.ForeColor = Color.White;
+            btnAgregar.IconChar = FontAwesome.Sharp.IconChar.File;
+            btnAgregar.IconColor = Color.White;
+            btnAgregar.IconFont = FontAwesome.Sharp.IconFont.Regular;
+            btnAgregar.IconSize = 24;
+            btnAgregar.ImageAlign = ContentAlignment.MiddleLeft;
+            btnAgregar.Location = new Point(91, 312);
+            btnAgregar.Margin = new Padding(3, 4, 3, 4);
+            btnAgregar.Name = "btnAgregar";
+            btnAgregar.Padding = new Padding(15, 0, 0, 0);
+            btnAgregar.Size = new Size(127, 74);
+            btnAgregar.TabIndex = 15;
+            btnAgregar.Text = "Guardar";
+            btnAgregar.UseVisualStyleBackColor = false;
+            btnAgregar.Click += btnAgregar_Click;
+            // 
+            // AgregarEditarPeriodoInscripcionView
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(224, 224, 224);
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnSalir);
+            Controls.Add(btnAgregar);
+            Controls.Add(txtNombre);
+            Controls.Add(LabelTitulo);
+            Controls.Add(chMaterias2doCuatrimestre);
+            Controls.Add(cboCiclosLectivos);
+            Controls.Add(label2);
+            Controls.Add(chInscripcionActiva);
+            Controls.Add(label1);
+            Name = "AgregarEditarPeriodoInscripcionView";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Agregar/Editar Periodo Inscripción";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private CheckBox chInscripcionActiva;
+        private ComboBox cboCiclosLectivos;
+        private Label label2;
+        private CheckBox chMaterias2doCuatrimestre;
+        private Label LabelTitulo;
+        private TextBox txtNombre;
+        private FontAwesome.Sharp.IconButton btnSalir;
+        private FontAwesome.Sharp.IconButton btnAgregar;
+    }
+}
---
+++ InstitutoDesktop/Views/Inscripciones/PeriodosInscripciones/AgregarEditarPeriodoInscripcionView.cs
@@ -0,0 +1,101 @@
+﻿using InstitutoDesktop.Services;
+using InstitutoServices.Interfaces;
+using InstitutoServices.Models.Inscripciones;
+using InstitutoServices.Services.Commons;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InstitutoDesktop.Views.Inscripciones
+{
+    public partial class AgregarEditarPeriodoInscripcionView : Form
+    {
+        private PeriodoInscripcion periodoInscripcion;
+        private readonly MemoryCacheServiceWinForms _memoryCache;
+        public AgregarEditarPeriodoInscripcionView(MemoryCacheServiceWinForms memoryCacheService)
+        {
+            InitializeComponent();
+            _memoryCache = memoryCacheService;
+            periodoInscripcion = periodoInscripcion;
+            CargarDatosCiclosLectivos();
+            CargarDatosPeriodoInscripcionAEditar();
+
+        }
+        public AgregarEditarPeriodoInscripcionView(MemoryCacheServiceWinForms memoryCacheService, PeriodoInscripcion periodoInscripcion)
+        {
+            InitializeComponent();
+            _memoryCache = memoryCacheService;
+            this.periodoInscripcion = null;
+            this.periodoInscripcion = periodoInscripcion;
+            CargarDatosCiclosLectivos();
+            CargarDatosPeriodoInscripcionAEditar();
+        }
+
+        private async void CargarDatosCiclosLectivos()
+        {
+            var ciclosLectivos = await _memoryCache.GetAllCacheAsync<CicloLectivo>("CiclosLectivos");
+            cboCiclosLectivos.DataSource = ciclosLectivos;
+            cboCiclosLectivos.DisplayMember = "Nombre";
+            cboCiclosLectivos.ValueMember = "Id";
+        }
+
+        private void CargarDatosPeriodoInscripcionAEditar()
+        {
+            if (periodoInscripcion == null)
+            {
+             
+                return;
+            }
+
+            txtNombre.Text = periodoInscripcion.Nombre;
+
+            if (periodoInscripcion.CicloLectivo != null)
+            {
+                cboCiclosLectivos.Text = periodoInscripcion.CicloLectivo.Nombre;
+            }
+            else
+            {
+                // Manejar el caso de null, tal vez establecer un valor predeterminado o registrar un error
+                cboCiclosLectivos.Text = string.Empty;
+            }
+
+            chInscripcionActiva.Checked = periodoInscripcion.InscripcionHabilitada;
+            chMaterias2doCuatrimestre.Checked = periodoInscripcion.SoloMaterias2doCuatrimestre;
+        }
+
+        private async void btnAgregar_Click(object sender, EventArgs e)
+        {
+            periodoInscripcion.Nombre = txtNombre.Text;
+            periodoInscripcion.CicloLectivo = (CicloLectivo)cboCiclosLectivos.SelectedItem;
+            periodoInscripcion.CicloLectivoId=(int)cboCiclosLectivos.SelectedValue;
+            periodoInscripcion.InscripcionHabilitada = chInscripcionActiva.Checked;
+            periodoInscripcion.SoloMaterias2doCuatrimestre = chMaterias2doCuatrimestre.Checked;
+
+
+            if (periodoInscripcion.Id == 0)
+            {
+                await _memoryCache.AddCacheAsync<PeriodoInscripcion>(periodoInscripcion, "PeriodosInscripciones");
+                //await ciclolectivoService.AddAsync(cicloLectivo);
+            }
+            else
+            {
+                await _memoryCache.UpdateCacheAsync<PeriodoInscripcion>(periodoInscripcion, "PeriodosInscripciones");
+                //await ciclolectivoService.UpdateAsync(cicloLectivo);
+            }
+
+            this.Close();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
+
